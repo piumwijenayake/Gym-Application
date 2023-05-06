d4a5ad135f290fa8d4ebeb61c37c94d8bc695c32
Piumi Wijenayake <wijenayakepiumi@gmail.com>
2023-05-06 21:48:21 +0530

    Initial Commit

---
+++ Gym Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gym Application
+//
+//  Created by Piumi Wijenayake on 2023-05-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
